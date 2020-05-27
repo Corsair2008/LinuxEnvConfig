c6b09aa986bfa2aba53f5abf28ef0590f6af090c
Wei Hong <wei.hong@amlogic.com>
2020-05-27 10:20:13 +0800

    vim: import leaderf config
    Signed-off-by: Wei Hong <wei.hong@amlogic.com>

--- envsetup.sh
+++ envsetup.sh
@@ -2,27 +2,43 @@
 
 CURDIR=`pwd`
 
-# backup vimrc
-if [ -f ~/.vimrc ]
-then
-	mv ~/.vimrc ~/.vimrc.backup
-fi
+import_config()
+{
+	# backup vimrc
+	if [ -f ~/.vimrc ]
+	then
+		mv ~/.vimrc ~/.vimrc.backup
+	fi
 
-ln -sf $CURDIR/vimrc ~/.vimrc
-ln -sf $CURDIR/vimrc.bundles ~/.vimrc.bundles
+	ln -sf $CURDIR/vimrc ~/.vimrc
+	ln -sf $CURDIR/vimrc.bundles ~/.vimrc.bundles
 
-# backup bashrc
-if [ -f ~/.bashrc ]
-then
-	mv ~/.bashrc ~/.bashrc.backup
-fi
+	# backup bashrc
+	if [ -f ~/.bashrc ]
+	then
+		mv ~/.bashrc ~/.bashrc.backup
+	fi
 
-ln -sf $CURDIR/bashrc ~/.bashrc
-ln -sf $CURDIR/tmux.conf ~/.tmux.conf
+	ln -sf $CURDIR/bashrc ~/.bashrc
+	ln -sf $CURDIR/tmux.conf ~/.tmux.conf
+}
 
-# install Monaco Font
-FONT_DIR=/usr/share/fonts/truetype/monaco/
+install_font()
+{
+	# install Monaco Font
+	if [ x$1 != x ]; then FONT_DIR=$1; else	FONT_DIR=/usr/share/fonts/truetype/monaco/; fi
 
-sudo mkdir -p $FONT_DIR
-sudo cp $CURDIR/Monaco_Linux.ttf $FONT_DIR
-sudo fc-cache -fv
+	sudo mkdir -p $FONT_DIR
+	sudo cp $CURDIR/Monaco_Linux.ttf $FONT_DIR
+	sudo fc-cache -fv
+}
+
+while getopts "f" opt; do
+	case $opt in
+	f)
+		install_font ;;
+	esac
+done
+
+import_config
+
